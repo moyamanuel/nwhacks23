406827a009533b7f375be006eb2f9617df93bb19
JA <82273499+swimmingHaribo@users.noreply.github.com>
2023-01-21 14:24:27 -0800

    Update files

---
+++ popup.js
@@ -0,0 +1,17 @@
+const timer = {
+    pomodoro: 25,
+    shortBreak: 5,
+    longBreak: 15,
+    longBreakInterval: 4,
+  };
+
+var short = document.getElementById("short");
+short.onclick = function(){
+
+}
+
+const btns = document.querySelector('');
+btns.addEventListener('click', handleBtns);
+function handleBtns(event){
+    const
+}

5fd36b9c46d4cd2fcbdf87dd0eca4a8fce0ee0b3
Manuel Moya <moya.manuel41@gmail.com>
2023-01-22 00:59:02 -0800

    Add timer functionality

---
+++ content.js
@@ -0,0 +1,18 @@
+document.getElementById("default").addEventListener("click", myFunction(300,document.getElementById('time-left')));
+function myFunction(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+

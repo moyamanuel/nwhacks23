6d04dec52a5b596818658cc36964e02de1cccc94
JA <82273499+swimmingHaribo@users.noreply.github.com>
2023-01-21 16:03:32 -0800

    Update files

--- popup.js
+++ popup.js
@@ -1,20 +1,26 @@
 const timer = {
     pomodoro: 25,
     shortBreak: 5,
-    longBreak: 15,
+    longBreak: 35,
     longBreakInterval: 4,
   };
 
+document.getElementById("default").onclick = function(){
+    startTimer(pomodoro,0);
+}
 document.getElementById("short").onclick = function(){
-
+    startTimer(shortBreak,0);
 }
 
 document.getElementById("long").onclick = function(){
-
+    startTimer(longBreak,0);
 }
 
-function startTimer(){
-    
+function startTimer(min, sec){
+    var min;
+    var sec;
+    const time = new String(min + ":" + sec);
+    document.getElementById("timer").innerHTML = time;
 }
 
 // three modes
